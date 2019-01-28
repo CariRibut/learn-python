ef10bc96eb07a9c80737a9fef21fcbdd0dbc5503
Satriadinata <35486119+CariRibut@users.noreply.github.com>
2019-01-28 08:28:40 +0700

    Update balik.py

--- balik.py
+++ balik.py
@@ -1,9 +1,10 @@
-nama = "kakak"
-nama1 = nama[::-1]
-print(nama1)
-nama2 = "kakak"
+nama = "Lemari"
+nama = nama[::-1].lower()
+print(nama)
+nama2 = "lemari"
+nama2=nama2.lower()
 print(nama2)
-if(nama2 == nama1 ):
+if(nama2 == nama ):
     print("Sama")
 else:
     print("Beda")

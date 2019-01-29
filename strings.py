0be6dfbcef0e851187b4a61900a279fa550d2fd3
Satriadinata <35486119+CariRibut@users.noreply.github.com>
2019-01-29 08:14:31 +0700

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,9 @@
+nama='Satria'
+umur = 20
+print("Nama saya %s .\nUmur saya %s"%(nama,umur))
+print("Bilangan desimal = %5.2f" %(13.4562))
+print("Bilangan desimal = %10.1f" %(13.4562))
+
+print('{} {} {}'.format('mokuzai','mencari','cinta'))
+print('{0:8} | {1:10}'.format('Apel','Banyak'))
+print('{0:8} | {1:10}'.format('Jeruk','Dikit'))
